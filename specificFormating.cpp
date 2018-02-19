e6f987af19f6620c7005cf7cc965d3cd656ed3bb
Valerie <valerielamm.com@gmail.com>
2018-02-18 22:12:18 -0600

    Info about oneself with specific formatting.

---
+++ specificFormating.cpp
@@ -0,0 +1,34 @@
+//
+//  Author : Valerie M Lamm
+//  12 September 2016
+//
+//
+//  <Purpose: Provide information about oneself in a specific format>
+//
+
+#include <iostream>
+
+
+int main() {
+    std::cout << "********************************************************************\n";
+    std::cout << "*\t\tWELCOME\t\t\t\t\t\t   *\n";
+    std::cout << "*\t\t\t\t\t\t\t\t   *\n";
+    std::cout << "*\tMy Name is : < Lamm Valerie M >\t\t\t\t   *\n";
+    std::cout << "*\tCourse No : < 1428 >\t\t\t\t\t   *\n";
+    std::cout << "*\tSection No : < 4 >\t\t\t\t\t   *\n";
+    std::cout << "*\tMajor : < Communication Design >\t\t\t   *\n";
+    std::cout << "*\tClassification : < Junior >\t\t\t\t   *\n";
+    std::cout << "*\tFull time / Part time : < Full time >\t\t\t   *\n";
+    std::cout << "*\tTaken this course before : < 'No' >\t\t\t   *\n";
+    std::cout << "*\tAbout Me :-\t\t\t\t\t\t   *\n";
+    std::cout << "*\t\tProgramming skills : < some >\t\t\t   *\n";
+    std::cout << "*\t\t\t\t\t\t\t\t   *\n";
+    std::cout << "*\t\tFavorite Movie : < Speak >\t\t\t   *\n";
+    std::cout << "*\t\tFavorite Sport : < Basketball >\t\t\t   *\n";
+    std::cout << "*\t\tFavorite TV Show : < Cheese in the Trap >\t   *\n";
+    std::cout << "*\t\tHobbies : < Guitar, Singing, Art >\t\t   *\n";
+    std::cout << "*\t\tEmployment : < Off Campus >\t\t\t   *\n";
+    std::cout << "*\t\t\t\t\t\t\t\t   *\n";
+    std::cout << "********************************************************************\n";
+    return 0;
+}
